71250d165b567d711ef8c1ffc243f5e8caf50f1d
Aniket Patil <128228805+AniketP04@users.noreply.github.com>
2023-09-27 13:16:37 +0530

    Update MiDaS.py

--- MiDaS.py
+++ MiDaS.py
@@ -1,39 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().system('pip install timm')
-
-
-# In[2]:
-
-
 import cv2
 import torch
 import matplotlib.pyplot as plt
 import timm
-
-
-# In[3]:
-
 
 midas = torch.hub.load('intel-isl/MiDaS', 'MiDaS_small')
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 midas.to(device)
 midas.eval()
 
-
-# In[4]:
-
-
 transforms = torch.hub.load('intel-isl/MiDaS', 'transforms')
 transform = transforms.small_transform
-
-
-# In[5]:
-
 
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
